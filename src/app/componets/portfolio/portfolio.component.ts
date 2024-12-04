47c4a557877c5e958f3d40f0d6171c95ae340f77
Gonza <gonzalodarkangel3@gmail.com>
2024-12-04 17:27:58 -0300

    Finalizado

--- src/app/componets/portfolio/portfolio.component.ts
+++ src/app/componets/portfolio/portfolio.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
+  VerLinkIn(): void {
+    const link = document.createElement('a');
+    link.href = 'https://github.com/GonzaloDark2/Gestion-Instituto-ProyectoFinal' ; // Ruta al archivo PDF dentro del proyecto Angular
+    link.target = '_blank'; // Abrir en una nueva pestaña
+    link.click();
+  }
+  CursoTICMAS(): void {
+    const link = document.createElement('a');
+    link.href = 'img/Curso-Ticmas.pdf'; // Ruta al archivo PDF dentro del proyecto Angular
+    link.download = 'Curso-Ticma-Gonzalo'; // Nombre del archivo descargado
+    link.click();
+  }
+  CursoMDP(): void {
+    const link = document.createElement('a');
+    link.href = 'img/Curso-MDP.pdf'; // Ruta al archivo PDF dentro del proyecto Angular
+    link.download = 'Curso-MDP-Gonzalo'; // Nombre del archivo descargado
+    link.click();
+  }
 
 }
